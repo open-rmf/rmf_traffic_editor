b8bba2770d2113c5182e7dee06c8e4e673aa00f7
Morgan Quigley <morgan@osrfoundation.org>
2019-11-18 21:14:42 +0800

    load previous project on startup if preferences checkbox is checked

---
+++ gui/preferences_keys.cpp
@@ -0,0 +1,10 @@
+#include "preferences_keys.h"
+
+const QString preferences_keys::thumbnail_path(
+    "editor/thumbnail_path");
+
+const QString preferences_keys::open_previous_file(
+    "editor/open_previous_file");
+
+const QString preferences_keys::previous_project_path(
+    "editor/previous_project_path");
---
+++ gui/preferences_keys.h
@@ -0,0 +1,14 @@
+#ifndef PREFERENCES_KEYS_H
+#define PREFERENCES_KEYS_H
+
+#include <QString>
+
+namespace preferences_keys {
+
+extern const QString thumbnail_path;
+extern const QString open_previous_file;
+extern const QString previous_project_path;
+
+};
+
+#endif
