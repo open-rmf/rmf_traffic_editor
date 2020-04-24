c279b3b4c816271191d34bcf747687d687a5a911
Yadunund <yadunund@openrobotics.org>
2020-04-24 15:23:03 +0800

    Added license info and header guard

--- building_sim_plugins/building_sim_common/include/building_sim_common/door_common.hpp
+++ building_sim_plugins/building_sim_common/include/building_sim_common/door_common.hpp
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2020 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef BUILDING_SIM_COMMON__DOOR_COMMON_HPP
+#define BUILDING_SIM_COMMON__DOOR_COMMON_HPP
+
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp/logger.hpp>
 
@@ -177,7 +197,7 @@ std::shared_ptr<DoorCommon> DoorCommon::make(
     joint_names.insert(right_door_joint_name);
 
   Doors doors;
-  
+
   auto extract_door = [&](SdfPtrT& joint_sdf)
   {
     auto joint_sdf_clone = joint_sdf->Clone();
@@ -237,3 +257,5 @@ std::shared_ptr<DoorCommon> DoorCommon::make(
 }
 
 } // namespace building_sim_common
+
+#endif // BUILDING_SIM_COMMON__DOOR_COMMON_HPP
