f6c198a7dc2baf49d763e15dde130d24b1ef266e
Yadu <yadunund@openrobotics.org>
2020-05-05 14:58:04 +0800

    Ign plugin door (#109)
    * WIP Skeleton of separation between gazebo and ignition plugins
    
    Signed-off-by: Luca Della Vedova <luca@openrobotics.org>
    
    * First draft of ignition plugins
    
    Signed-off-by: Luca Della Vedova <luca@openrobotics.org>
    
    * Remove unexisting files from CMakeLists
    
    Signed-off-by: Luca Della Vedova <luca@openrobotics.org>
    
    * WIP migrating control, sdf and TF2 logic to common class
    
    Signed-off-by: Luca Della Vedova <luca@openrobotics.org>
    
    * First draft of fully separated slotcar library
    
    Signed-off-by: Luca Della Vedova <luca@openrobotics.org>
    
    * Cleanups and moving more ROS logic to common library
    
    Signed-off-by: Luca Della Vedova <luca@openrobotics.org>
    
    * WIP porting ignition slotcar plugin
    
    Signed-off-by: Luca Della Vedova <luca@openrobotics.org>
    
    * RMF linter
    
    * Renamed slotcar.cpp to slotcar_common.cpp in building_sim_common
    
    * Corrections after improvements to style guide
    
    * Modular door for gazebo working
    
    * Modular door for ign working
    
    * Format fixes
    
    * Fixed build errors
    
    * Refactored gazebo door plugin
    
    * Refactored ign door plugin
    
    * Passing arguments by ref and removed unused functions
    
    * Refactor
    
    * Removed shared pointer for DoorElement
    
    * Using sdf clone inside make()
    
    * Modified DoorCommon constructor
    
    * Added license info and header guard
    
    Co-authored-by: Luca Della Vedova <luca@openrobotics.org>

--- building_sim_plugins/building_sim_gazebo/src/door.cpp
+++ building_sim_plugins/building_sim_gazebo/src/door.cpp
@@ -5,123 +5,34 @@
 #include <gazebo_ros/node.hpp>
 #include <rclcpp/rclcpp.hpp>
 
-#include <rmf_door_msgs/msg/door_mode.hpp>
-#include <rmf_door_msgs/msg/door_state.hpp>
-#include <rmf_door_msgs/msg/door_request.hpp>
-
 #include <building_sim_common/utils.hpp>
+#include <building_sim_common/door_common.hpp>
 
 using namespace building_sim_common;
 
 namespace building_sim_gazebo {
-
 //==============================================================================
-class Door
-{
-public:
-
-  bool _debuggable;
-
-  Door(const bool debuggable,
-    const gazebo::physics::JointPtr& joint,
-    const MotionParams& params,
-    const bool flip_direction = false)
-  : _debuggable(debuggable),
-    _joint(joint),
-    _params(params)
-  {
-    if (flip_direction)
-    {
-      _closed_position = _joint->LowerLimit(0);
-      _open_position = _joint->UpperLimit(0);
-    }
-    else
-    {
-      _closed_position = _joint->UpperLimit(0);
-      _open_position = _joint->LowerLimit(0);
-    }
-  }
-
-  bool is_open() const
-  {
-    return std::abs(_open_position - _joint->Position(0)) <= _params.dx_min;
-  }
-
-  bool is_closed() const
-  {
-    return std::abs(_closed_position - _joint->Position(0)) <= _params.dx_min;
-  }
-
-  void open(double dt)
-  {
-    _set_door_command(_open_position, dt);
-  }
-
-  void close(double dt)
-  {
-    _set_door_command(_closed_position, dt);
-  }
-
-
-private:
-
-  void _set_door_command(const double target, const double dt)
-  {
-    double dx = target - _joint->Position(0);
-
-    if (std::abs(dx) < _params.dx_min/2.0)
-      dx = 0.0;
-
-    const double door_v = compute_desired_rate_of_change(
-      dx, _joint->GetVelocity(0), _params, dt);
-
-    _joint->SetParam("vel", 0, door_v);
-    _joint->SetParam("fmax", 0, _params.f_max);
-  }
-
-  gazebo::physics::JointPtr _joint;
-  MotionParams _params;
-
-  double _open_position;
-  double _closed_position;
-
-};
 
 class DoorPlugin : public gazebo::ModelPlugin
 {
 private:
   gazebo::event::ConnectionPtr _update_connection;
-  gazebo_ros::Node::SharedPtr _ros_node;
   gazebo::physics::ModelPtr _model;
+  std::unordered_map<std::string, gazebo::physics::JointPtr> _joints;
 
-  using DoorMode = rmf_door_msgs::msg::DoorMode;
-  using DoorState = rmf_door_msgs::msg::DoorState;
-  rclcpp::Publisher<DoorState>::SharedPtr _door_state_pub;
-
-  using DoorRequest = rmf_door_msgs::msg::DoorRequest;
-  rclcpp::Subscription<DoorRequest>::SharedPtr _door_request_sub;
-
-  std::vector<Door> _doors;
-
-  DoorState _state;
-  DoorRequest _request;
-
-  double _last_update_time;
-  double _last_pub_time;
+  std::shared_ptr<DoorCommon> _door_common = nullptr;
 
   bool _initialized = false;
 
 public:
   DoorPlugin()
-  : _last_update_time(0),
-    _last_pub_time(0)
   {
     // Do nothing
   }
 
   void Load(gazebo::physics::ModelPtr model, sdf::ElementPtr sdf) override
   {
-    _ros_node = gazebo_ros::Node::Get(sdf);
+    auto _ros_node = gazebo_ros::Node::Get(sdf);
     _model = model;
 
     RCLCPP_INFO(
@@ -129,122 +40,38 @@ public:
       "Loading DoorPlugin for [%s]",
       _model->GetName().c_str());
 
-    MotionParams params;
-    get_sdf_param_if_available<double>(sdf, "v_max_door", params.v_max);
-    get_sdf_param_if_available<double>(sdf, "a_max_door", params.a_max);
-    get_sdf_param_if_available<double>(sdf, "a_nom_door", params.a_nom);
-    get_sdf_param_if_available<double>(sdf, "dx_min_door", params.dx_min);
-    get_sdf_param_if_available<double>(sdf, "f_max_door", params.f_max);
+    _door_common = DoorCommon::make(
+      _model->GetName(),
+      _ros_node,
+      sdf);
 
-    sdf::ElementPtr door_element;
-    std::string left_door_joint_name;
-    std::string right_door_joint_name;
-    std::string door_type;
-    if (!get_element_required(sdf, "door", door_element) ||
-      !get_sdf_attribute_required<std::string>(
-        door_element, "left_joint_name", left_door_joint_name) ||
-      !get_sdf_attribute_required<std::string>(
-        door_element, "right_joint_name", right_door_joint_name) ||
-      !get_sdf_attribute_required<std::string>(
-        door_element, "type", door_type))
-    {
-      RCLCPP_ERROR(
-        _ros_node->get_logger(),
-        " -- Missing required parameters for [%s] plugin",
-        _model->GetName().c_str());
+    if (!_door_common)
       return;
-    }
 
-    if (left_door_joint_name == "empty_joint" &&
-      right_door_joint_name == "empty_joint")
+    for (const auto& joint_name : _door_common->joint_names())
     {
-      RCLCPP_ERROR(
-        _ros_node->get_logger(),
-        " -- Both door joint names are missing for [%s] plugin, at least one"
-        " is required", _model->GetName().c_str());
-      return;
-    }
-
-    if (left_door_joint_name != "empty_joint")
-    {
-      const auto left_door_joint = _model->GetJoint(left_door_joint_name);
-      if (!left_door_joint)
+      const auto joint = _model->GetJoint(joint_name);
+      if (!joint)
       {
-        RCLCPP_ERROR(
-          _ros_node->get_logger(),
-          " -- Model is missing the left door joint [%s]",
-          left_door_joint_name.c_str());
+        RCLCPP_ERROR(_ros_node->get_logger(),
+          " -- Model is missing the joint [%s]",
+          joint_name.c_str());
         return;
       }
-      _doors.emplace_back(_model->GetName() == "chart_lift_door",
-        left_door_joint, params);
+      _joints.insert(std::make_pair(joint_name, joint));
     }
 
-    if (right_door_joint_name != "empty_joint")
-    {
-      const auto right_door_joint = _model->GetJoint(right_door_joint_name);
-      if (!right_door_joint)
-      {
-        RCLCPP_ERROR(
-          _ros_node->get_logger(),
-          " -- Model is missing the right door joint [%s]",
-          right_door_joint_name.c_str());
-        return;
-      }
-      _doors.emplace_back(_model->GetName() == "chart_lift_door",
-        right_door_joint, params, true);
-    }
+    _initialized = true;
 
     _update_connection = gazebo::event::Events::ConnectWorldUpdateBegin(
       std::bind(&DoorPlugin::on_update, this));
 
-    _door_state_pub = _ros_node->create_publisher<DoorState>(
-      "/door_states", rclcpp::SystemDefaultsQoS());
-
-    _door_request_sub = _ros_node->create_subscription<DoorRequest>(
-      "/door_requests", rclcpp::SystemDefaultsQoS(),
-      [&](DoorRequest::UniquePtr msg)
-      {
-        if (msg->door_name == _state.door_name)
-          _request = *msg;
-      });
-
-    _state.door_name = model->GetName();
-
-    // Set the mode to closed by default
-    _request.requested_mode.value = DoorMode::MODE_CLOSED;
-
-    _initialized = true;
-
-    RCLCPP_INFO(
-      _ros_node->get_logger(),
+    RCLCPP_INFO(_ros_node->get_logger(),
       "Finished loading [%s]",
       _model->GetName().c_str());
   }
 
 private:
-
-  bool all_doors_open() const
-  {
-    for (const auto& door : _doors)
-    {
-      if (!door.is_open())
-        return false;
-    }
-
-    return true;
-  }
-
-  bool all_doors_closed() const
-  {
-    for (const auto& door : _doors)
-    {
-      if (!door.is_closed())
-        return false;
-    }
-
-    return true;
-  }
 
   void on_update()
   {
@@ -252,43 +79,32 @@ private:
       return;
 
     const double t = _model->GetWorld()->SimTime().Double();
-    const double dt = t - _last_update_time;
-    _last_update_time = t;
 
-    if (_request.requested_mode.value == DoorMode::MODE_OPEN)
+    // Create DoorUpdateRequest
+    std::vector<DoorCommon::DoorUpdateRequest> requests;
+    for (const auto& joint : _joints)
     {
-      for (auto& door : _doors)
-        door.open(dt);
-    }
-    else
-    {
-      for (auto& door : _doors)
-        door.close(dt);
+      DoorCommon::DoorUpdateRequest request;
+      request.joint_name = joint.first;
+      request.position = joint.second->Position(0);
+      request.velocity = joint.second->GetVelocity(0);
+      requests.push_back(request);
     }
 
-    if (t - _last_pub_time >= 1.0)
+    auto results = _door_common->update(t, requests);
+
+    // Apply motions to the joints
+    for (const auto& result : results)
     {
-      _last_pub_time = t;
-
-      _state.door_time = _ros_node->now();
-      if (all_doors_open())
-      {
-        _state.current_mode.value = DoorMode::MODE_OPEN;
-      }
-      else if (all_doors_closed())
-      {
-        _state.current_mode.value = DoorMode::MODE_CLOSED;
-      }
-      else
-      {
-        _state.current_mode.value = DoorMode::MODE_MOVING;
-      }
-
-      _door_state_pub->publish(_state);
+      const auto it = _joints.find(result.joint_name);
+      assert(it != _joints.end());
+      it->second->SetParam("vel", 0, result.velocity);
+      it->second->SetParam("fmax", 0, result.fmax);
     }
   }
 
 };
 
 GZ_REGISTER_MODEL_PLUGIN(DoorPlugin)
+
 } // namespace building_sim_gazebo
--- building_sim_plugins/building_sim_ign/src/door.cpp
+++ building_sim_plugins/building_sim_ign/src/door.cpp
@@ -1,9 +1,3 @@
-//#include <gazebo/common/Plugin.hh>
-//#include <gazebo/physics/Model.hh>
-//#include <gazebo/physics/World.hh>
-//#include <gazebo/physics/Joint.hh>
-//#include <gazebo_ros/node.hpp>
-
 #include <ignition/plugin/Register.hh>
 
 #include <ignition/gazebo/System.hh>
@@ -15,11 +9,8 @@
 
 #include <rclcpp/rclcpp.hpp>
 
-#include <rmf_door_msgs/msg/door_mode.hpp>
-#include <rmf_door_msgs/msg/door_state.hpp>
-#include <rmf_door_msgs/msg/door_request.hpp>
-
 #include <building_sim_common/utils.hpp>
+#include <building_sim_common/door_common.hpp>
 
 // TODO remove this
 using namespace ignition;
@@ -31,106 +22,6 @@ using namespace building_sim_common;
 namespace building_sim_ign {
 
 //==============================================================================
-class Door
-{
-public:
-
-  bool _debuggable;
-
-  Door(const bool debuggable,
-    const Entity& entity,
-    EntityComponentManager& ecm,
-    const MotionParams& params,
-    const bool flip_direction = false)
-  : _debuggable(debuggable),
-    _entity(entity),
-    _params(params)
-  {
-    // Create components for joint position, velocity and velocitycmd
-    if (!ecm.EntityHasComponentType(entity,
-      components::JointPosition().TypeId()))
-      ecm.CreateComponent(entity, components::JointPosition({0}));
-    if (!ecm.EntityHasComponentType(entity,
-      components::JointVelocity().TypeId()))
-      ecm.CreateComponent(entity, components::JointVelocity({0}));
-    if (!ecm.EntityHasComponentType(entity,
-      components::JointVelocityCmd().TypeId()))
-      ecm.CreateComponent(entity, components::JointVelocityCmd({0}));
-    auto axis = ecm.Component<components::JointAxis>(entity)->Data();
-    auto lower = axis.Lower();
-    auto upper = axis.Upper();
-    if (flip_direction)
-    {
-      _closed_position = lower;
-      _open_position = upper;
-    }
-    else
-    {
-      _closed_position = upper;
-      _open_position = lower;
-    }
-  }
-
-  bool is_open(EntityComponentManager& ecm) const
-  {
-    auto pos = _get_door_position(ecm);
-    return std::abs(_open_position - pos) <= _params.dx_min;
-  }
-
-  bool is_closed(EntityComponentManager& ecm) const
-  {
-    auto pos = _get_door_position(ecm);
-    return std::abs(_closed_position - pos) <= _params.dx_min;
-  }
-
-  void open(EntityComponentManager& ecm, double dt)
-  {
-    _set_door_command(ecm, _open_position, dt);
-  }
-
-  void close(EntityComponentManager& ecm, double dt)
-  {
-    _set_door_command(ecm, _closed_position, dt);
-  }
-
-
-private:
-
-  void _set_door_command(EntityComponentManager& ecm, const double target,
-    const double dt)
-  {
-    double dx = target - _get_door_position(ecm);
-    auto vel_cmd = ecm.Component<components::JointVelocityCmd>(_entity);
-
-    if (std::abs(dx) < _params.dx_min/2.0)
-      dx = 0.0;
-
-    const double door_v = compute_desired_rate_of_change(
-      dx, _get_door_velocity(ecm), _params, dt);
-
-    vel_cmd->Data()[0] = door_v;
-    // TODO f_max in the SDF file
-    //_joint->SetParam("fmax", 0, _params.f_max);
-  }
-
-  // TODO template these two functions
-  double _get_door_position(EntityComponentManager& ecm) const
-  {
-    return ecm.Component<components::JointPosition>(_entity)->Data()[0];
-  }
-
-  double _get_door_velocity(EntityComponentManager& ecm) const
-  {
-    return ecm.Component<components::JointVelocity>(_entity)->Data()[0];
-  }
-
-  Entity _entity;
-  MotionParams _params;
-
-  double _open_position;
-  double _closed_position;
-
-};
 
 class IGNITION_GAZEBO_VISIBLE DoorPlugin
   : public System,
@@ -139,27 +30,27 @@ class IGNITION_GAZEBO_VISIBLE DoorPlugin
 {
 private:
   rclcpp::Node::SharedPtr _ros_node;
-  Model _model;
+  std::unordered_map<std::string, Entity> _joints;
 
-  using DoorMode = rmf_door_msgs::msg::DoorMode;
-  using DoorState = rmf_door_msgs::msg::DoorState;
-  rclcpp::Publisher<DoorState>::SharedPtr _door_state_pub;
-
-  using DoorRequest = rmf_door_msgs::msg::DoorRequest;
-  rclcpp::Subscription<DoorRequest>::SharedPtr _door_request_sub;
-
-  std::vector<Door> _doors;
-
-  DoorState _state;
-  DoorRequest _request;
-
-  double _last_pub_time;
+  std::shared_ptr<DoorCommon> _door_common = nullptr;
 
   bool _initialized = false;
 
+  void create_entity_components(Entity entity, EntityComponentManager& ecm)
+  {
+    if (!ecm.EntityHasComponentType(entity,
+      components::JointPosition().TypeId()))
+        ecm.CreateComponent(entity, components::JointPosition({0}));
+    if (!ecm.EntityHasComponentType(entity,
+      components::JointVelocity().TypeId()))
+        ecm.CreateComponent(entity, components::JointVelocity({0}));
+    if (!ecm.EntityHasComponentType(entity,
+      components::JointVelocityCmd().TypeId()))
+        ecm.CreateComponent(entity, components::JointVelocityCmd({0}));
+  }
+
 public:
   DoorPlugin()
-  : _last_pub_time(0)
   {
     // TODO init ros node
     // Do nothing
@@ -172,101 +63,45 @@ public:
     //_ros_node = gazebo_ros::Node::Get(sdf);
     // TODO get properties from sdf instead of hardcoded (will fail for multiple instantiations)
     // TODO proper rclcpp init (only once and pass args)
-    _model = Model(entity);
+    auto model = Model(entity);
     char const** argv = NULL;
     if (!rclcpp::is_initialized())
       rclcpp::init(0, argv);
-    std::string plugin_name("plugin_" + _model.Name(ecm));
+    std::string plugin_name("plugin_" + model.Name(ecm));
     ignwarn << "Initializing plugin with name " << plugin_name << std::endl;
     _ros_node = std::make_shared<rclcpp::Node>(plugin_name);
 
-    RCLCPP_INFO(
-      _ros_node->get_logger(),
+    RCLCPP_INFO(_ros_node->get_logger(),
       "Loading DoorPlugin for [%s]",
-      _model.Name(ecm).c_str());
-    auto sdfClone = sdf->Clone();
+      model.Name(ecm).c_str());
 
-    MotionParams params;
-    get_sdf_param_if_available<double>(sdfClone, "v_max_door", params.v_max);
-    get_sdf_param_if_available<double>(sdfClone, "a_max_door", params.a_max);
-    get_sdf_param_if_available<double>(sdfClone, "a_nom_door", params.a_nom);
-    get_sdf_param_if_available<double>(sdfClone, "dx_min_door", params.dx_min);
-    // TODO add to traffic_editor
-    //get_sdf_param_if_available<double>(sdf, "f_max_door", params.f_max);
+    _door_common = DoorCommon::make(
+      model.Name(ecm),
+      _ros_node,
+      sdf);
 
-    sdf::ElementPtr door_element;
-    std::string left_door_joint_name;
-    std::string right_door_joint_name;
-    std::string door_type;
-    if (!get_element_required(sdfClone, "door", door_element) ||
-      !get_sdf_attribute_required<std::string>(
-        door_element, "left_joint_name", left_door_joint_name) ||
-      !get_sdf_attribute_required<std::string>(
-        door_element, "right_joint_name", right_door_joint_name) ||
-      !get_sdf_attribute_required<std::string>(
-        door_element, "type", door_type))
-    {
-      RCLCPP_ERROR(
-        _ros_node->get_logger(),
-        " -- Missing required parameters for [%s] plugin",
-        _model.Name(ecm).c_str());
+    if (!_door_common)
       return;
-    }
 
-    if (left_door_joint_name != "empty_joint")
+    for (const auto& joint_name : _door_common->joint_names())
     {
-      const auto left_door_joint =
-        _model.JointByName(ecm, left_door_joint_name);
-      if (!left_door_joint)
+      const auto joint = model.JointByName(ecm, joint_name);
+      if (!joint)
       {
-        RCLCPP_ERROR(
-          _ros_node->get_logger(),
-          " -- Model is missing the left door joint [%s]",
-          left_door_joint_name.c_str());
+        RCLCPP_ERROR(_ros_node->get_logger(),
+          " -- Model is missing the joint [%s]",
+          joint_name.c_str());
         return;
       }
-      _doors.emplace_back(_model.Name(ecm) == "chart_lift_door",
-        left_door_joint, ecm, params);
+      create_entity_components(joint, ecm);
+      _joints.insert({joint_name, joint});
     }
-
-    if (right_door_joint_name != "empty_joint")
-    {
-      const auto right_door_joint = _model.JointByName(ecm,
-          right_door_joint_name);
-      if (!right_door_joint)
-      {
-        RCLCPP_ERROR(
-          _ros_node->get_logger(),
-          " -- Model is missing the right door joint [%s]",
-          right_door_joint_name.c_str());
-        return;
-      }
-      _doors.emplace_back(_model.Name(ecm) == "chart_lift_door",
-        right_door_joint, ecm, params, true);
-    }
-
-    _door_state_pub = _ros_node->create_publisher<DoorState>(
-      "/door_states", rclcpp::SystemDefaultsQoS());
-
-    _door_request_sub = _ros_node->create_subscription<DoorRequest>(
-      "/door_requests", rclcpp::SystemDefaultsQoS(),
-      [&](DoorRequest::UniquePtr msg)
-      {
-        if (msg->door_name == _state.door_name)
-          _request = *msg;
-      });
-
-    _state.door_name = _model.Name(ecm);
-
-    // Set the mode to closed by default
-    _request.requested_mode.value = DoorMode::MODE_CLOSED;
 
     _initialized = true;
 
-    RCLCPP_INFO(
-      _ros_node->get_logger(),
+    RCLCPP_INFO(_ros_node->get_logger(),
       "Finished loading [%s]",
-      _model.Name(ecm).c_str());
+      model.Name(ecm).c_str());
   }
 
   void PreUpdate(const UpdateInfo& info, EntityComponentManager& ecm) override
@@ -275,70 +110,37 @@ public:
     rclcpp::spin_some(_ros_node);
     if (!_initialized)
       return;
-    double dt =
-      (std::chrono::duration_cast<std::chrono::nanoseconds>(info.dt).count())
-      * 1e-9;
 
     double t =
       (std::chrono::duration_cast<std::chrono::nanoseconds>(info.simTime).
       count()) * 1e-9;
 
-    if (_request.requested_mode.value == DoorMode::MODE_OPEN)
+    // Create DoorUpdateRequest
+    std::vector<DoorCommon::DoorUpdateRequest> requests;
+    for (const auto& joint : _joints)
     {
-      for (auto& door : _doors)
-        door.open(ecm, dt);
+      DoorCommon::DoorUpdateRequest request;
+      request.joint_name = joint.first;
+      request.position = ecm.Component<components::JointPosition>(
+          joint.second)->Data()[0];
+      request.velocity = ecm.Component<components::JointVelocity>(
+          joint.second)->Data()[0];
+      requests.push_back(request);
     }
-    else
+    
+    auto results = _door_common->update(t, requests);
+
+    // Apply motions to the joints
+    for (const auto& result : results)
     {
-      for (auto& door : _doors)
-        door.close(ecm, dt);
-    }
-
-    if (t - _last_pub_time >= 1.0)
-    {
-      _last_pub_time = t;
-
-      _state.door_time = _ros_node->now();
-      if (all_doors_open(ecm))
-      {
-        _state.current_mode.value = DoorMode::MODE_OPEN;
-      }
-      else if (all_doors_closed(ecm))
-      {
-        _state.current_mode.value = DoorMode::MODE_CLOSED;
-      }
-      else
-      {
-        _state.current_mode.value = DoorMode::MODE_MOVING;
-      }
-
-      _door_state_pub->publish(_state);
+      const auto it = _joints.find(result.joint_name);
+      assert(it != _joints.end());
+      auto vel_cmd = ecm.Component<components::JointVelocityCmd>(
+        it->second);
+      vel_cmd->Data()[0] = result.velocity;
     }
   }
-
-private:
-
-  bool all_doors_open(EntityComponentManager& ecm) const
-  {
-    for (const auto& door : _doors)
-    {
-      if (!door.is_open(ecm))
-        return false;
-    }
-
-    return true;
-  }
-
-  bool all_doors_closed(EntityComponentManager& ecm) const
-  {
-    for (const auto& door : _doors)
-    {
-      if (!door.is_closed(ecm))
-        return false;
-    }
-
-    return true;
-  }
+  
 };
 
 IGNITION_ADD_PLUGIN(
