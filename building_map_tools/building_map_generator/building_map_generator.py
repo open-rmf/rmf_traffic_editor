7beb5479b9e9b09a3cd7b043473e31f7cb5bd2d3
methylDragon <methylDragon@gmail.com>
2020-06-09 15:39:28 +0800

    Implement namespace support when generating worlds

--- building_map_tools/building_map_generator/building_map_generator.py
+++ building_map_tools/building_map_generator/building_map_generator.py
@@ -7,6 +7,8 @@ try:
 except Exception:
     from _init_argparse import parser
 
+from pprint import pprint
+
 import pit_crew
 import logging
 import sys
@@ -25,34 +27,59 @@ def main():
     if args.command == "gazebo":
         # Construct model set
         model_set = set()
+        stringent_dict = {}  # Dict to tighten download scope
 
         if not args.no_download:
             building = g.parse_editor_yaml(args.INPUT)
 
             for _, level in building.levels.items():
                 for model in level.models:
-                    model_set.add(model.model_name)
+                    if "/" in model.model_name:
+                        model_name = "".join(model.model_name.split("/")[1:])
+                        author_name = model.model_name.split("/")[0]
+
+                        model_set.add((model_name, author_name))
+                        stringent_dict[model_name.lower()] = \
+                            author_name.lower()
+                    else:
+                        model_set.add(model.model_name)
 
         missing_models = pit_crew.get_missing_models(
             model_set,
             model_path=args.model_path,
-            cache_file_path=args.cache
+            cache_file_path=args.cache,
+            lower=True
         )
 
+        logger.info("\n== REQUESTED MODEL REPORT ==")
+        pprint(missing_models)
+
+        logger.info("\n== THE FOLLOWING MODELS HAVE SPECIFIED AUTHORS ==")
+        pprint(stringent_dict)
+        print()
+
         for downloadable_model in missing_models['downloadable']:
-            model_name, author_name = downloadable_model
+            model_name, author_names = downloadable_model
 
-            # TODO: For now it just picks the first author
-            # When model authors are put into the thumbnails, then we can
-            # use that instead
-            logger.info("\nDownloading %s by %s from Fuel"
-                        % (model_name, author_name[0]))
+            if model_name in stringent_dict:
+                author_name = stringent_dict[model_name]
+                logger.info("\nDownloading %s by %s from Fuel"
+                            % (model_name, author_name))
+            else:
+                author_name = author_names[0]
+                logger.info("\nDownloading %s by %s from Fuel"
+                            % (model_name, author_name))
 
-            pit_crew.download_model(model_name, author_name[0],
+                logger.warning("No author specified for model '%s', "
+                               "using first valid author '%s'" %
+                               (model_name, author_name))
+
+            pit_crew.download_model(model_name, author_name,
                                     download_path=args.model_path)
 
-        logger.warning("\nUnavailable models (not in local or Fuel): %s"
-                       % missing_models['missing'])
+        if missing_models['missing']:
+            logger.warning("\n\Missing models (not in local or Fuel):")
+            pprint(missing_models['missing'])
 
         g.generate_gazebo_sdf(
             args.INPUT,
