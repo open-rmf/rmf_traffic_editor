1eca3575e68ad03e333668caec1452551131b825
MakinoharaShouko <qianj4@uw.edu>
2020-08-20 10:34:55 -0700

    style

--- building_map_tools/building_map/level.py
+++ building_map_tools/building_map/level.py
@@ -104,9 +104,9 @@ class Level:
     def set_lift_vert_lists(self, lift_vert_lists, lifts):
         for lift_name, lift in lifts.items():
             if self.elevation >= lift.lowest_elevation and \
-                self.elevation <= lift.highest_elevation:
-                    self.lift_vert_lists[lift_name] = \
-                        (lift_vert_lists[lift_name])
+                    self.elevation <= lift.highest_elevation:
+                self.lift_vert_lists[lift_name] = \
+                    (lift_vert_lists[lift_name])
 
     def calculate_scale_using_measurements(self):
         # use the measurements to estimate scale for this level
