268e121f0638ac6e07e0801267734ccbb00ce53f
youliang <tan_you_liang@hotmail.com>
2020-09-10 17:23:20 +0800

    tested and works well! minor changes on readability

--- building_sim_plugins/building_gazebo_plugins/src/toggle_floors.cpp
+++ building_sim_plugins/building_gazebo_plugins/src/toggle_floors.cpp
@@ -13,18 +13,17 @@
 
 #include <string>
 #include <unordered_map>
+
 using std::string;
 using FleetState = rmf_fleet_msgs::msg::FleetState;
 using RobotState = rmf_fleet_msgs::msg::RobotState;
-
 
 class ToggleFloors : public gazebo::GUIPlugin
 {
   Q_OBJECT
   gazebo::transport::NodePtr node;
   gazebo::transport::PublisherPtr visual_pub;
-  std::unordered_map<string, bool> floor_states;
-  std::unordered_map<string, bool> robot_states;
+  std::unordered_map<string, std::atomic<bool>> floor_visibility;
   gazebo_ros::Node::SharedPtr ros_node;
   rclcpp::Subscription<FleetState>::SharedPtr fleet_state_sub;
 
@@ -46,6 +45,8 @@ public:
   {
     printf("ToggleFloors::Load()\n");
     ros_node = gazebo_ros::Node::Get(sdf);
+
+    // toggle non-static robots
     fleet_state_sub = ros_node->create_subscription<FleetState>(
       "/fleet_states", rclcpp::SystemDefaultsQoS(),
       [&](FleetState::UniquePtr msg)
@@ -55,15 +56,10 @@ public:
         visual_msg.set_parent_name("world");
         for (const RobotState& robot : msg->robots)
         {
-          visible = floor_states[robot.location.level_name];
-          if (robot_states.find(robot.name) == robot_states.end() ||
-            robot_states[robot.name] != visible)
-          {
-            robot_states[robot.name] = visible;
-            visual_msg.set_name(robot.name);
-            visual_msg.set_visible(visible);
-            visual_pub->Publish(visual_msg);
-          }
+          visible = floor_visibility[robot.location.level_name];
+          visual_msg.set_name(robot.name);
+          visual_msg.set_visible(visible);
+          visual_pub->Publish(visual_msg);
         }
       });
 
@@ -87,7 +83,7 @@ public:
           models.push_back(model_ele->GetAttribute("name")->GetAsString());
         model_ele = model_ele->GetNextElement("model");
       }
-      floor_states[floor_name] = true;
+      floor_visibility[floor_name] = true;
 
       printf(
         "ToggleFloors::Load found a floor element: [%s]->[%s]\n",
@@ -117,7 +113,7 @@ public:
     std::vector<string> models)
   {
     bool visible = button->isChecked();
-    floor_states[floor_name] = visible;
+    floor_visibility[floor_name] = visible;
     printf(
       "clicked: [%s] %s\n",
       model_name.c_str(),
