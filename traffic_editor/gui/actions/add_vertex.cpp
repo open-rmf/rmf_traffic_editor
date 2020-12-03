5f3c93401f4ce739c553e7f6e8b29476a54a6e63
Arjo Chakravarty <arjo@openrobotics.org>
2020-12-03 17:12:27 +0800

    fix code style issues

--- traffic_editor/gui/actions/add_vertex.cpp
+++ traffic_editor/gui/actions/add_vertex.cpp
@@ -1,6 +1,7 @@
 #include "add_vertex.h"
 
-AddVertexCommand::AddVertexCommand(Project *project, EditorModeId mode, int level_idx, double x, double y) {
+AddVertexCommand::AddVertexCommand(Project *project, EditorModeId mode, int level_idx, double x, double y) 
+{
   _mode = mode;
   _x = x;
   _y = y;
@@ -8,12 +9,16 @@ AddVertexCommand::AddVertexCommand(Project *project, EditorModeId mode, int leve
   _project = project;
 }
 
-void AddVertexCommand::undo() {
-  if (_mode == MODE_BUILDING || _mode == MODE_TRAFFIC) {
+void AddVertexCommand::undo() 
+{
+  if (_mode == MODE_BUILDING || _mode == MODE_TRAFFIC) 
+  {
     size_t length = _project->building.levels[_level_idx].vertices.size();
     //TODO: SLOW O(n) method... Need to rework datastructures.
-    for(size_t i = 0; i < length; i++){
-      if(_project->building.levels[_level_idx].vertices[i].uuid == _vert_id) {
+    for(size_t i = 0; i < length; i++)
+    {
+      if(_project->building.levels[_level_idx].vertices[i].uuid == _vert_id) 
+      {
         _project->building.levels[_level_idx].vertices.erase(
           _project->building.levels[_level_idx].vertices.begin()+i);
         break;
@@ -22,8 +27,10 @@ void AddVertexCommand::undo() {
   }
 }
 
-void AddVertexCommand::redo() {
-    if (_mode == MODE_BUILDING || _mode == MODE_TRAFFIC) {
+void AddVertexCommand::redo() 
+{
+    if (_mode == MODE_BUILDING || _mode == MODE_TRAFFIC) 
+    {
         _project->building.add_vertex(_level_idx, _x, _y);
         size_t sz = _project->building.levels[_level_idx].vertices.size();
         _vert_id = _project->building.levels[_level_idx].vertices[sz-1].uuid;
--- traffic_editor/gui/actions/add_vertex.h
+++ traffic_editor/gui/actions/add_vertex.h
@@ -1,12 +1,16 @@
+#ifndef _ADD_VERTEX_H_
+#define _ADD_VERTEX_H_
+
 #include <QUndoCommand>
 #include "editor_mode_id.h"
 #include "project.h"
 
-class AddVertexCommand: public QUndoCommand {
+class AddVertexCommand: public QUndoCommand 
+{
 
 public:
   AddVertexCommand(Project *project, EditorModeId mode, int level_idx, double x, double y);
-  virtual ~AddVertexCommand() {};
+  virtual ~AddVertexCommand() {}
   
 
   void undo() override;
@@ -14,9 +18,11 @@ public:
   void redo() override;
 
 private:
-  Project *_project;
+  Project* _project;
   EditorModeId _mode;
-  double _x , _y;
+  double _x, _y;
   int _level_idx; 
   QUuid _vert_id;
 };
+
+#endif
