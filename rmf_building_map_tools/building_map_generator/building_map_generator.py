68c3b4f2c2d8d2d343522befed19b7d6318699ae
Morgan Quigley <morgan@osrfoundation.org>
2021-11-04 17:05:54 +0800

    first attempt at navgraph visualization model generator
    Signed-off-by: Morgan Quigley <morgan@osrfoundation.org>

--- rmf_building_map_tools/building_map_generator/building_map_generator.py
+++ rmf_building_map_tools/building_map_generator/building_map_generator.py
@@ -32,5 +32,9 @@ def main():
         g.generate_nav(args.INPUT, args.OUTPUT_DIR)
 
 
+    if args.command == "navgraph_visualization":
+        g.generate_navgraph_visualization(args.INPUT, args.OUTPUT_DIR)
+
+
 if __name__ == "__main__":
     main()
